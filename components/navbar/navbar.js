f5460d8ff4bd972eed71679d9df2ad7f866f2ea2
JaeWon Baek <bjw1622@gmail.com>
2023-01-12 15:04:11 +0900

    navbar 추가

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,5 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
 import styles from "./navbar.module.scss";
 const Navbar = () => {
-  return;
+  return (
+    <div id={styles.nav}>
+      <div id={styles.navlogo}>
+        <Link href="/">
+          <Image src="/images/logo.png" alt="" width={80} height={80}></Image>
+        </Link>
+        <Link className={styles.navlink} href="/home">
+          <div id={styles.navtitle}>
+            <h1>해</h1>
+            <h1 id={styles.titleone}>?</h1>
+            <h1>마</h1>
+            <h1 id={styles.titletwo}>!</h1>
+          </div>
+        </Link>
+      </div>
+      <div id={styles.navuser}>
+        <ul id={styles.navuserinfo}>
+          <li className={styles.navli}>
+            <Link className={styles.navlink} href="/home">
+              대시 보드
+            </Link>
+          </li>
+          <li className={styles.navli}>
+            <Link className={styles.navlink} href="/login">
+              로그인
+            </Link>
+          </li>
+          <li className={styles.navli}>
+            <Link className={styles.navlink} href="/home">
+              로그아웃
+            </Link>
+          </li>
+          <li className={styles.navli}>
+            <Link className={styles.navlink} href="/home">
+              마이페이지
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 export default Navbar;
