b31bdc1deb74eec3ca062605004ecdf59ee5dd63
JaeWon Baek <bjw1622@gmail.com>
2023-01-13 17:52:34 +0900

    navbar session 유무 수정

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -5,43 +5,76 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 const Navbar = () => {
   const { data: session } = useSession();
-
-  return (
-    <div id={styles.nav}>
-      <div id={styles.navlogo}>
-        <Link href="/">
-          <Image src="/images/logo.png" alt="" width={80} height={80}></Image>
-        </Link>
-        <Link className={styles.navlink} href="/home">
-          <div id={styles.navtitle}>
-            <h1>해</h1>
-            <h1 id={styles.titleone}>?</h1>
-            <h1>마</h1>
-            <h1 id={styles.titletwo}>!</h1>
-          </div>
-        </Link>
+  if (session) {
+    return (
+      <div id={styles.nav}>
+        <div id={styles.navlogo}>
+          <Link href="/home">
+            <Image src="/images/logo.png" alt="" width={80} height={80}></Image>
+          </Link>
+          <Link className={styles.navlink} href="/home">
+            <div id={styles.navtitle}>
+              <h1>해</h1>
+              <h1 id={styles.titleone}>?</h1>
+              <h1>마</h1>
+              <h1 id={styles.titletwo}>!</h1>
+            </div>
+          </Link>
+        </div>
+        <div id={styles.navuser}>
+          <ul id={styles.navuserinfo}>
+            <li className={styles.navli}>
+              <h5>
+                안녕하세요, <Link href="/home">{session.user.name}</Link>님
+              </h5>
+            </li>
+            <li className={styles.navli}>
+              <Link className={styles.navlink} href="/home">
+                대시 보드
+              </Link>
+            </li>
+            <li className={styles.navli}>
+              <Link className={styles.navlink} href="/home">
+                마이페이지
+              </Link>
+            </li>
+            <li className={styles.navli}>
+              <button onClick={() => signOut()}>Sign out</button>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div id={styles.navuser}>
-        <ul id={styles.navuserinfo}>
-          <li className={styles.navli}>
-            <Link className={styles.navlink} href="/home">
-              대시 보드
-            </Link>
-          </li>
-          <li className={styles.navli}>
-            <button onClick={() => signIn()}>Sign in</button>
-          </li>
-          <li className={styles.navli}>
-            <button onClick={() => signOut()}>Sign out</button>
-          </li>
-          <li className={styles.navli}>
-            <Link className={styles.navlink} href="/home">
-              마이페이지
-            </Link>
-          </li>
-        </ul>
+    );
+  } else {
+    return (
+      <div id={styles.nav}>
+        <div id={styles.navlogo}>
+          <Link href="/">
+            <Image src="/images/logo.png" alt="" width={80} height={80}></Image>
+          </Link>
+          <Link className={styles.navlink} href="/home">
+            <div id={styles.navtitle}>
+              <h1>해</h1>
+              <h1 id={styles.titleone}>?</h1>
+              <h1>마</h1>
+              <h1 id={styles.titletwo}>!</h1>
+            </div>
+          </Link>
+        </div>
+        <div id={styles.navuser}>
+          <ul id={styles.navuserinfo}>
+            <li className={styles.navli}>
+              <Link className={styles.navlink} href="/home">
+                대시 보드
+              </Link>
+            </li>
+            <li className={styles.navli}>
+              <button onClick={() => signIn()}>Sign in</button>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 export default Navbar;
