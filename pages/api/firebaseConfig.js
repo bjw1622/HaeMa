50bd961e42277bb782265763458fae92a0f73285
JaeWon Baek <bjw1622@gmail.com>
2023-01-25 10:50:03 +0900

    firebaseConfig 수정

--- pages/api/firebaseConfig.js
+++ pages/api/firebaseConfig.js
@@ -5,16 +5,12 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyD3FeIl2plQFBpnqltN_et7rGdOblNvqgE",
   authDomain: "hae-ma.firebaseapp.com",
+  databaseURL:
+    "https://hae-ma-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "hae-ma",
   storageBucket: "hae-ma.appspot.com",
   messagingSenderId: "831224162773",
   appId: "1:831224162773:web:4e4fe2dd466094ff23b55c",
-  measurementId: "G-8L8N9D12SF",
-  databaseUrl:
-    "https://hae-ma-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
-
-const app = initializeApp(firebaseConfig);
-export const firebaseApp = app;
-export const fbAuth = getAuth(app);
-export const db = getFirestore(app);
+export const fbAuth = getAuth(initializeApp(firebaseConfig));
+export const db = getFirestore(initializeApp(firebaseConfig));
