61aaa5a6df3ce4736af5a5df43d9f126f58a9454
JaeWon Baek <bjw1622@gmail.com>
2023-01-25 15:51:52 +0900

    post Api 추가

--- pages/write/index.js
+++ pages/write/index.js
@@ -1,96 +1,70 @@
+import { useSession } from "next-auth/react";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useState } from "react";
 import styles from "./index.module.scss";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../api/firebaseConfig";
 const Write = () => {
-  useEffect(() => {
-    const btnBold = document.getElementById("btn-bold");
-    const btnItalic = document.getElementById("btn-italic");
-    const btnUnderline = document.getElementById("btn-underline");
-    const btnStrike = document.getElementById("btn-strike");
-    const btnOrderedList = document.getElementById("btn-ordered-list");
-    const btnUnorderedList = document.getElementById("btn-unordered-list");
-
-    btnBold.addEventListener("click", function () {
-      setStyle("bold");
-    });
-
-    btnItalic.addEventListener("click", function () {
-      setStyle("italic");
-    });
-
-    btnUnderline.addEventListener("click", function () {
-      setStyle("underline");
-    });
-
-    btnStrike.addEventListener("click", function () {
-      setStyle("strikeThrough");
-    });
-
-    btnOrderedList.addEventListener("click", function () {
-      setStyle("insertOrderedList");
-    });
-
-    btnUnorderedList.addEventListener("click", function () {
-      setStyle("insertUnorderedList");
-    });
-
-    function setStyle(style) {
-      document.execCommand(style);
-    }
-  }, []);
-
-  return (
-    <div id={styles.write}>
-      <div className={styles.writeinfo}>
-        <label className={styles.writelabel} htmlFor="">
-          제목
-        </label>
-        <input
-          className={styles.writeinputs}
-          type="text"
-          placeholder="해?마!"
-        />
-      </div>
-
-      <div className={styles.writeinfo}>
-        <label className={styles.writelabel} htmlFor="">
-          작성자
-        </label>
-        <input
-          className={styles.writeinputs}
-          type="text"
-          placeholder="관리자"
-          readOnly
-        />
-      </div>
-
-      <div>
-        <label className={styles.writelabel} htmlFor="">
-          내용
-        </label>
-        <div className={styles.buttontools}>
-          <button id="btn-bold">
-            <b>B</b>
-          </button>
-          <button id="btn-italic">
-            <i>I</i>
-          </button>
-          <button id="btn-underline">
-            <u>U</u>
-          </button>
-          <button id="btn-strike">
-            <s>S</s>
-          </button>
-          <button id="btn-ordered-list">OL</button>
-          <button id="btn-unordered-list">UL</button>
-          <button id="btn-image">IMG</button>
+  const { data: session } = useSession();
+  const [titleValue, setTitleValue] = useState("");
+  const [writeValue, setwriteValue] = useState("");
+  if (session) {
+    const titleInput = (e) => {
+      setTitleValue(e.target.value);
+    };
+    const writeInput = (e) => {
+      setwriteValue(e.currentTarget.textContent);
+    };
+    const addBoard = async () => {
+      const todoListCollectionCollectionRef = collection(db, "Board");
+      await addDoc(todoListCollectionCollectionRef, {
+        title: titleValue,
+        content: writeValue,
+        email: session.user.email,
+        writer: session.user.name,
+        date: serverTimestamp(),
+        hae: 0,
+        ma: 0,
+        viewCount: 0,
+      });
+    };
+    return (
+      <div id={styles.write}>
+        <div className={styles.writeinfo}>
+          <label className={styles.writelabel} htmlFor="">
+            제목
+          </label>
+          <input
+            onChange={titleInput}
+            className={styles.writeinputs}
+            type="text"
+          />
         </div>
-        <div id={styles.writeinput} contentEditable="true"></div>
+        <div className={styles.writeinfo}>
+          <label className={styles.writelabel} htmlFor="">
+            작성자
+          </label>
+          <div className={styles.writeinputs} type="text" readOnly>
+            {session.user.name}
+          </div>
+        </div>
+        <div>
+          <label className={styles.writelabel} htmlFor="">
+            내용
+          </label>
+          <div
+            id={styles.writeinput}
+            onInput={writeInput}
+            contentEditable="true"
+          ></div>
+        </div>
+        <Link href={"/"}>
+          <button id={styles.writebutton} onClick={addBoard}>
+            글작성
+          </button>
+        </Link>
       </div>
-      <Link href={"/home"}>
-        <button id={styles.writebutton}>글작성</button>
-      </Link>
-    </div>
-  );
+    );
+  }
 };
 export default Write;
