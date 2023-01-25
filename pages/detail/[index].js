34cad53cd70a3b3141b675102185d56d01020ec5
JaeWon Baek <bjw1622@gmail.com>
2023-01-25 17:19:15 +0900

    detail router 추가

---
+++ pages/detail/[index].js
@@ -0,0 +1,34 @@
+import { doc, getDoc } from "firebase/firestore";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Content from "../../components/Detail/Content";
+import HaeMa from "../../components/Detail/HaeMa";
+import Reply from "../../components/Detail/Reply";
+import ReplyList from "../../components/Detail/ReplyList";
+import Title from "../../components/Detail/Title";
+import { db } from "../api/firebaseConfig";
+import styles from "./index.module.scss";
+const Detail = () => {
+  const router = useRouter();
+  const routerIndex = router.query.index;
+  const [board, setBoard] = useState([]);
+  const getBoard = async () => {
+    const docRef = doc(db, "Board", routerIndex);
+    const docSnap = await getDoc(docRef);
+    setBoard(docSnap.data());
+  };
+  useEffect(() => {
+    if (!router.isReady) return;
+    getBoard();
+  }, [router.isReady]);
+  return (
+    <div className={styles.detail}>
+      <Title title={board.title} writer={board.writer} />
+      <Content content={board.content} />
+      <HaeMa hae={board.hae} ma={board.ma} />
+      <Reply />
+      <ReplyList />
+    </div>
+  );
+};
+export default Detail;
