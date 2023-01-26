afe0d0113cafe48e2a600914e6e2b1c4e4a0979a
JaeWon Baek <bjw1622@gmail.com>
2023-01-26 10:40:51 +0900

    router 추가

--- components/Detail/HaeMa.js
+++ components/Detail/HaeMa.js
@@ -1,13 +1,23 @@
+import { useRouter } from "next/router";
 import styles from "./index.module.scss";
 const HaeMa = (props) => {
+  const router = useRouter();
+  const routerIndex = router.query.index;
+  const clickHae = (e) => {};
+  const clickMa = (e) => {};
+  console.log(routerIndex);
   return (
     <div id={styles.recommand}>
       <div className={styles.btnDiv}>
-        <button id={styles.doBtn}>해?</button>
+        <button id={styles.doBtn} onClick={clickHae}>
+          해?
+        </button>
         <h3>{props.hae}</h3>
       </div>
       <div className={styles.btnDiv}>
-        <button id={styles.dontBtn}>마!</button>
+        <button id={styles.dontBtn} onClick={clickMa}>
+          마!
+        </button>
         <h3>{props.ma}</h3>
       </div>
     </div>
