d321e006a70211e039c8ce454990c2df524a7650
JaeWon Baek <bjw1622@gmail.com>
2023-01-30 16:01:59 +0900

    app.js loading useEffect 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import Layout from "../components/layout";
 import "../styles/global.css";
 import { SessionProvider } from "next-auth/react";
+import { useEffect, useState } from "react";
+import Loading from "../components/Loading";
+import { Router } from "next/router";
 function MyApp({ session, Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+    const end = () => {
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+  }, []);
   return (
     <SessionProvider session={session}>
       <Layout>
-        <Component {...pageProps} />
+        {loading ? <Loading></Loading> : <Component {...pageProps} />}
       </Layout>
     </SessionProvider>
   );
