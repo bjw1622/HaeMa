8bc99072fd50733f0dfc58147d04231f772a7d87
JaeWon Baek <bjw1622@gmail.com>
2023-01-30 10:53:28 +0900

    댓글 작성 api 추가

--- components/Detail/Reply.js
+++ components/Detail/Reply.js
@@ -1,13 +1,38 @@
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { db } from "../../pages/api/firebaseConfig";
 import styles from "./index.module.scss";
-const Reply = () => {
+const Reply = (props) => {
+  const { data: session } = useSession();
+  const [inputReply, setInputReply] = useState("");
+  const textInputValue = (event) => {
+    setInputReply(event.target.value);
+  };
+  const addReply = async () => {
+    const replyCollection = collection(db, "Reply");
+    await addDoc(replyCollection, {
+      boardId: props.boardId,
+      content: inputReply,
+      date: serverTimestamp(),
+      email: session.user.email,
+      writer: session.user.name,
+      parentReplyNo: 0,
+    });
+    setInputReply("");
+  };
   return (
     <div id={styles.reply}>
       <textarea
         id={styles.replyArea}
         placeholder="댓글을 입력해주세요"
         rows="5"
+        onChange={textInputValue}
+        value={inputReply}
       ></textarea>
-      <button id={styles.replyBtn}>작성</button>
+      <button id={styles.replyBtn} onClick={addReply}>
+        작성
+      </button>
     </div>
   );
 };
