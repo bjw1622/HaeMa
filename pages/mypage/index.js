0c411cb555a59546040673a06d2da7d7450d108e
JaeWon Baek <bjw1622@gmail.com>
2023-01-31 16:35:13 +0900

    mypage api 호출

--- pages/mypage/index.js
+++ pages/mypage/index.js
@@ -1,8 +1,16 @@
-import { useSession } from "next-auth/react";
+import {
+  collection,
+  getCountFromServer,
+  query,
+  where,
+} from "firebase/firestore";
+import { getSession, useSession } from "next-auth/react";
 import ProfileImage from "../../components/ProfileImage/ProfileImage";
+import { db } from "../api/firebaseConfig";
 import styles from "./index.module.scss";
-const MyPage = () => {
+const MyPage = ({ boardCount, replyCount }) => {
   const { data: session } = useSession();
+
   if (session) {
     return (
       <div id={styles.mypage}>
@@ -10,11 +18,11 @@ const MyPage = () => {
         <div id={styles.activeinfo}>
           <div>
             <h4>작성 게시글 수</h4>
-            <h5>3</h5>
+            <h5>{boardCount}</h5>
           </div>
           <div>
             <h4>작성 댓글 수</h4>
-            <h5>3</h5>
+            <h5>{replyCount}</h5>
           </div>
         </div>
         <h3 className={styles.headtext}>{">"} 프로필 이미지 변경</h3>
@@ -23,4 +31,28 @@ const MyPage = () => {
     );
   }
 };
+
+export async function getServerSideProps(context) {
+  const sessionData = await getSession(context);
+
+  const collBoard = collection(db, "Board");
+  const boardCountQuery = query(
+    collBoard,
+    where("email", "==", `${sessionData.user.email}`)
+  );
+  const boardCountSnapShot = await getCountFromServer(boardCountQuery);
+  const boardCount = boardCountSnapShot.data().count;
+
+  const collReply = collection(db, "Reply");
+  const replyCountQuery = query(
+    collReply,
+    where("email", "==", `${sessionData.user.email}`)
+  );
+  const replyCountSnapShot = await getCountFromServer(replyCountQuery);
+  const replyCount = replyCountSnapShot.data().count;
+  return {
+    props: { boardCount: boardCount, replyCount: replyCount },
+  };
+}
+
 export default MyPage;
