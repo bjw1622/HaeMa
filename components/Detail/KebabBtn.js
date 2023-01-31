443b4032ff10c1a82ff9b4c06af49729d5d6d829
JaeWon Baek <bjw1622@gmail.com>
2023-01-31 13:53:13 +0900

    Kebab 컴포넌트 분리

--- components/Detail/KebabBtn.js
+++ components/Detail/KebabBtn.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Menu, MenuDivider, MenuGroup, MenuItem } from "@szhsin/react-menu";
+import { Menu, MenuGroup, MenuButton } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
 import styles from "./index.module.scss";
 const KebabBtn = (props) => {
@@ -10,14 +10,14 @@ const KebabBtn = (props) => {
       menuButton={<button id={styles.menuBtn}>...</button>}
     >
       <MenuGroup id={styles.menuGroup}>
-        <MenuItem className={styles.groupBtn}>수정</MenuItem>
-        <MenuItem
+        <MenuButton className={styles.groupBtn}>수정</MenuButton>
+        <MenuButton
           className={styles.groupBtn}
           keydata={props.keydata}
           onClick={props.remove}
         >
           삭제
-        </MenuItem>
+        </MenuButton>
       </MenuGroup>
     </Menu>
   );
