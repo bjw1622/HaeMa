1375f392fa5fe6cd7c64709b6825a73ad22acb80
JaeWon Baek <bjw1622@gmail.com>
2023-02-01 15:48:51 +0900

    getServerSideProps 추가

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,76 @@
-import Finder from "../components/Main/Finder";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import PagingBtn from "../components/Main/PagingBtn";
 import Table from "../components/Main/Table";
 import WriteBtn from "../components/Main/WriteBtn";
-const Home = () => {
+import { db } from "./api/firebaseConfig";
+import styles from "../components/Main/index.module.scss";
+import { useState } from "react";
+const Home = ({ getBoardList }) => {
+  const [selectValue, setSelectValue] = useState("title");
+
+  const [findValue, setFindValue] = useState("");
+
+  const [boardListValue, setBoardListValue] = useState([]);
+
+  const [findCheck, setFindCheck] = useState(false);
+  const select = (e) => {
+    setSelectValue(e.target.value);
+  };
+
+  const findInput = (e) => {
+    setFindValue(e.target.value);
+  };
+
+  const find = async () => {
+    const boardListCollectionCollectionRef = collection(db, "Board");
+    const data = await getDocs(
+      query(
+        boardListCollectionCollectionRef,
+        where(`${selectValue}`, "==", `${findValue}`),
+        orderBy("date")
+      )
+    );
+    setBoardListValue(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    setFindCheck(true);
+  };
+
   return (
     <>
-      <Finder />
-      <Table />
+      <div className={styles.find}>
+        <select onChange={select}>
+          <option>title</option>
+          <option>writer</option>
+        </select>
+        <div>
+          <input onChange={findInput} value={findValue} type="text" />
+          <button onClick={find}>검색</button>
+        </div>
+      </div>
+      <Table
+        boardList={getBoardList}
+        boardListValue={boardListValue}
+        findCheck={findCheck}
+      />
       <WriteBtn />
       <PagingBtn />
     </>
   );
 };
+
+export async function getServerSideProps() {
+  const boardListCollectionCollectionRef = collection(db, "Board");
+  let list;
+  const getBoards = async () => {
+    const data = await getDocs(
+      query(boardListCollectionCollectionRef, orderBy("date"))
+    );
+    list = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    return list;
+  };
+  return {
+    props: {
+      getBoardList: JSON.parse(JSON.stringify(await getBoards())),
+    },
+  };
+}
 export default Home;
