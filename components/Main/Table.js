0f133476902af7d0c9be06e516d60a3c6637b8ae
JaeWon Baek <bjw1622@gmail.com>
2023-02-01 15:48:08 +0900

    검색 조건 분기

--- components/Main/Table.js
+++ components/Main/Table.js
@@ -1,52 +1,72 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { db } from "../../pages/api/firebaseConfig";
 import styles from "./index.module.scss";
-const Table = () => {
-  const [todoList, setTodoList] = useState([]);
-  const todoListCollectionCollectionRef = collection(db, "Board");
-  const getBoards = async () => {
-    const data = await getDocs(
-      query(todoListCollectionCollectionRef, orderBy("date"))
+const Table = (props) => {
+  if (props.findCheck) {
+    return (
+      <div className={styles.home}>
+        <table className={styles.table}>
+          <thead className={styles.thead}>
+            <tr>
+              <th className={styles.th}>#</th>
+              <th className={styles.th}>제목</th>
+              <th className={styles.th}>작성자</th>
+              <th className={styles.th}>날짜</th>
+              <th className={styles.th}>조회수</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.boardListValue.map((item, num) => {
+              return (
+                <tr key={item.id}>
+                  <td className={styles.td}>{num + 1}</td>
+                  <td className={styles.td}>
+                    <Link className={styles.link} href={`/detail/${item.id}`}>
+                      {item.title}
+                    </Link>
+                  </td>
+                  <td className={styles.td}>{item.writer}</td>
+                  <td className={styles.td}></td>
+                  <td className={styles.td}>{item.viewCount}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     );
-    setTodoList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  };
-  useEffect(() => {
-    getBoards();
-  }, []);
-  return (
-    <div className={styles.home}>
-      <table className={styles.table}>
-        <thead className={styles.thead}>
-          <tr>
-            <th className={styles.th}>#</th>
-            <th className={styles.th}>제목</th>
-            <th className={styles.th}>작성자</th>
-            <th className={styles.th}>날짜</th>
-            <th className={styles.th}>조회수</th>
-          </tr>
-        </thead>
-        <tbody>
-          {todoList.map((item, num) => {
-            return (
-              <tr key={item.id}>
-                <td className={styles.td}>{num + 1}</td>
-                <td className={styles.td}>
-                  <Link className={styles.link} href={`/detail/${item.id}`}>
-                    {item.title}
-                  </Link>
-                </td>
-                <td className={styles.td}>{item.writer}</td>
-                <td className={styles.td}></td>
-                <td className={styles.td}>{item.viewCount}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+  } else {
+    return (
+      <div className={styles.home}>
+        <table className={styles.table}>
+          <thead className={styles.thead}>
+            <tr>
+              <th className={styles.th}>#</th>
+              <th className={styles.th}>제목</th>
+              <th className={styles.th}>작성자</th>
+              <th className={styles.th}>날짜</th>
+              <th className={styles.th}>조회수</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.boardList.map((item, num) => {
+              return (
+                <tr key={item.id}>
+                  <td className={styles.td}>{num + 1}</td>
+                  <td className={styles.td}>
+                    <Link className={styles.link} href={`/detail/${item.id}`}>
+                      {item.title}
+                    </Link>
+                  </td>
+                  <td className={styles.td}>{item.writer}</td>
+                  <td className={styles.td}></td>
+                  <td className={styles.td}>{item.viewCount}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 };
-
 export default Table;
