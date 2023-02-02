906152cb187cfa74c39647e6b94a0304e3da686d
JaeWon Baek <bjw1622@gmail.com>
2023-02-02 10:43:15 +0900

    게시글 삭제 시 댓글 삭제

--- components/Detail/Title.js
+++ components/Detail/Title.js
@@ -1,16 +1,30 @@
 import styles from "./index.module.scss";
 import TitleKebabBtn from "./TitleKebabBtn";
 import { db } from "../../pages/api/firebaseConfig";
-import { collection, deleteDoc, query, where } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 
 const Title = (props) => {
   const removeBoard = async () => {
-    // if (window.confirm("삭제 하시겠습니까?")) {
-    const replyListColleciton = await deleteDoc(
-      query(collection(db, "Reply"), where("boardId", "==", `${props.boardId}`))
-    );
-    // }
-    console.log(props);
+    // 댓글 삭제
+    if (window.confirm("삭제 하시겠습니까?")) {
+      const boardListCollectionCollectionRef = collection(db, "Reply");
+      const replyListQuery = query(
+        boardListCollectionCollectionRef,
+        where("boardId", "==", `${props.boardId}`)
+      );
+      const data = await getDocs(replyListQuery);
+      if (data.docs.length !== 0) {
+        data.docs.map((item) => {
+          deleteDoc(item.ref);
+        });
+      }
+    }
   };
   return (
     <div id={styles.detailTitle}>
